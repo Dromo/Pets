a4de509c5093895021e10fde823a6c1cb43331bb
Dromo <4rudolf@gmail.com>
2025-06-15 16:03:15 +0200

    update workflow

--- update_lotro_interface.py
+++ update_lotro_interface.py
@@ -2,16 +2,19 @@ import httpx
 import requests
 import os
 
+# Get the pasword from github secret
 try:
     MD5PASSWORD = os.environ["MD5PASSWORD"]
 except KeyError:
     raise Exception("Secret password not available!")
 
+# Get the update version
 try:
     update = os.environ['UPDATE']
 except KeyError:
     raise Exception("Update variable not available!")
 
+# Login lotrointerface.com
 with httpx.Client() as client:
     post_data = {
         "vb_login_username": "drono",
@@ -20,11 +23,14 @@ with httpx.Client() as client:
     }
     login_response = client.post('https://www.lotrointerface.com/forums/login.php', data=post_data)
 
+    # Succesfully logged in lotrointerface
     if login_response.status_code == 200:
+        # get the zip file from the newly created release
         zip_url = 'https://github.com/Dromo/Pets/releases/download/'+update+'/Pets_'+update+'.zip'
         response = requests.get(zip_url)
         if response.status_code != 200:
             raise Exception("Failed to fetch zip")
+        # setup lotrointerface edit form values
         files = {
             'replacementfile': ('Pets_'+update+'.zip', response.content, 'application/zip')
         }
@@ -34,9 +40,9 @@ with httpx.Client() as client:
             'type': '1',
             'ftitle': 'Pets '+update,
             'version': update[1:],
-            'fileaction': 'keep',
-#            'fileaction': 'replace',
+            'fileaction': 'replace', # or 'keep'
         }
+        # get and convert Release.md from github syntax to lotrointerface syntax
         raw_url = "https://raw.githubusercontent.com/Dromo/Pets/refs/heads/master/Release.md"
         response = requests.get(raw_url)
         if response.status_code == 200:
@@ -50,6 +56,7 @@ with httpx.Client() as client:
 
         edit_url = 'https://www.lotrointerface.com/downloads/editfile.php'
 
+        # attempt to edit description and uplaod to lotrointerface
         response = client.post(edit_url, data=data, files=files)
 
         if response.status_code == 200:
